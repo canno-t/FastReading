65ab3d7d782d81da1e99dada3799454a29956e22
canno-t <janpekalski939@gmail.com>
2024-08-04 18:39:32 +0200

    feat(006)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server:{
+        host: '0.0.0.0',
+        hmr: {
+            clientPort: 3000,
+            host: 'localhost',
+            protocol: 'ws'
+        },
+        port: 3000,
+        watch: {
+            usePolling: true
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/game.js,', 'resources/css/game.css'],
